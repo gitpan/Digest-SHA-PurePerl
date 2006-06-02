64f977a8ecbd63768d5d2ed56ae9dcef4af72517
Mark Shelor <mshelor@cpan.org>
2006-06-02 21:45:18 +0000

    Import of MSHELOR/Digest-SHA-PurePerl-5.40 from CPAN.
    gitpan-cpan-distribution: Digest-SHA-PurePerl
    gitpan-cpan-version:      5.40
    gitpan-cpan-path:         MSHELOR/Digest-SHA-PurePerl-5.40.tar.gz
    gitpan-cpan-author:       MSHELOR
    gitpan-cpan-maturity:     released

--- PurePerl.pm
+++ PurePerl.pm
@@ -6,7 +6,7 @@ use strict;
 use warnings;
 use integer;
 
-our $VERSION = '5.39';
+our $VERSION = '5.40';
 
 require Exporter;
 our @ISA = qw(Exporter);
@@ -849,7 +849,8 @@ sub _addfile {
 sub _Addfile {
 	my ($self, $file, $mode) = @_;
 
-	if (ref(\$file) eq 'GLOB') { return(_addfile($self, $file)) }
+	return(_addfile($self, $file)) if
+		ref($file) eq 'GLOB' || ref(\$file) eq 'GLOB';
 
 	$mode = defined($mode) ? $mode : "";
 	my ($binary, $portable) = map { $_ eq $mode } ("b", "p");
